b03845649d7806f160f8454cdb9e8db34c8435aa
telamonnaxeno <telamonnaxeno@hotmail.com>
2024-04-29 11:47:43 +0800

    Updated migrations for ERD model

--- database/migrations/2999_03_27_99_assign_constraints.php
+++ database/migrations/2999_03_27_99_assign_constraints.php
@@ -85,6 +85,47 @@ return new class extends Migration
             $table->foreign('module_question_id')->references('id')->on('module_questions')->onDelete('cascade');
         });
 
+        //CHAPTERS
+        Schema::table('chapters', function (Blueprint $table) {
+            // Foreign key relationship with the modules table
+            $table->foreign('module_id')->references('id')->on('modules')->onDelete('cascade');            
+        });
+
+        //ITEM
+        Schema::table('item', function (Blueprint $table) {
+            // Foreign key relationship with the chapters table
+            $table->foreign('chapter_id')->references('id')->on('chapters')->onDelete('cascade');            
+        });
+
+        //ASSIGNED MODULE
+        Schema::table('assigned_module', function (Blueprint $table) {
+            // Foreign key relationship with the company users table
+            $table->foreign('UserID')->references('UserID')->on('companyusers')->onDelete('cascade');
+            $table->foreign('CompanyID')->references('CompanyID')->on('companyusers')->onDelete('cascade'); 
+            // Foreign key relationship with the modules table
+            $table->foreign('ModuleID')->references('id')->on('modules')->onDelete('cascade');      
+        });
+
+        //CHAPTER PROGRESS
+        Schema::table('chapters_progress', function (Blueprint $table) {
+            // Foreign key relationship with the assigned modules table
+            $table->foreign('UserID')->references('UserID')->on('assigned_module')->onDelete('cascade');
+            $table->foreign('CompanyID')->references('CompanyID')->on('assigned_module')->onDelete('cascade'); 
+            $table->foreign('ModuleID')->references('ModuleID')->on('assigned_module')->onDelete('cascade');   
+            // Foreign key relationship with the assigned chapters table   
+            $table->foreign('ChapterID')->references('id')->on('chapters')->onDelete('cascade');  
+        });
+
+        //ITEM PROGRESS
+        Schema::table('item_progress', function (Blueprint $table) {
+            // Foreign key relationship with the assigned modules table
+            $table->foreign('UserID')->references('UserID')->on('assigned_module')->onDelete('cascade');
+            $table->foreign('CompanyID')->references('CompanyID')->on('assigned_module')->onDelete('cascade'); 
+            $table->foreign('ModuleID')->references('ModuleID')->on('assigned_module')->onDelete('cascade');   
+            // Foreign key relationship with the assigned item table   
+            $table->foreign('ItemID')->references('id')->on('item')->onDelete('cascade');  
+        });
+
     }
 
     /**
@@ -138,6 +179,56 @@ return new class extends Migration
             $table->dropForeign(['UserID']);
             $table->dropForeign(['CompanyID']);
             $table->dropForeign(['PostID']);
+        });
+
+        
+        //MODULES
+        Schema::table('modules', function (Blueprint $table) {
+            $table->dropForeign(['CompanyID']);
+        });
+        
+        //MODULE QUESTIONS
+        Schema::table('module_questions', function (Blueprint $table) {
+            $table->dropForeign(['module_id']);
+        });
+
+        //USER RESPONSE
+        Schema::table('user_responses', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['module_question_id']);
+        });
+
+        //CHAPTERS
+        Schema::table('chapters', function (Blueprint $table) {
+            $table->dropForeign(['module_id']);            
+        });
+
+        //ITEM
+        Schema::table('item', function (Blueprint $table) {
+            $table->dropForeign(['chapter_id']);            
+        });
+
+        //ASSIGNED MODULE
+        Schema::table('assigned_module', function (Blueprint $table) {
+            $table->dropForeign(['UserID']);
+            $table->dropForeign(['CompanyID']); 
+            $table->dropForeign(['ModuleID']);      
+        });
+
+        //CHAPTER PROGRESS
+        Schema::table('chapters_progress', function (Blueprint $table) {
+            $table->dropForeign(['UserID']);
+            $table->dropForeign(['CompanyID']); 
+            $table->dropForeign(['ModuleID']);   
+            $table->dropForeign(['ChapterID']);  
+        });
+
+        //ITEM PROGRESS
+        Schema::table('item_progress', function (Blueprint $table) {
+            $table->dropForeign(['UserID']);
+            $table->dropForeign(['CompanyID']); 
+            $table->dropForeign(['ModuleID']);   
+            $table->dropForeign(['ItemID']);  
         });
 
         // //Turn back on foreign key checks
