8c5ce61ee1db1fa735e8d2a8cc048c4eb8a66bd3
telamonnaxeno <telamonnaxeno@hotmail.com>
2024-05-30 17:30:00 +0800

    Added tracking for multi-tenancy subscription

--- database/migrations/2024_01_26_081916_create_companies_table.php
+++ database/migrations/2024_01_26_081916_create_companies_table.php
@@ -19,6 +19,16 @@ return new class extends Migration
             $table->string('Industry');
             $table->text('Address');
             $table->string('Website');
+
+            //Multi-Tenancy Feature
+            $table->string('sidebar_color')->nullable();
+            $table->string('button_color')->nullable();
+            $table->string('company_logo')->nullable();
+
+            //Subscription Feature
+            $table->timestamp('subscription_starts_at')->nullable();
+            $table->timestamp('subscription_ends_at')->nullable();
+
             $table->timestamps();
             //Soft Delete Column
             $table->softDeletes();
