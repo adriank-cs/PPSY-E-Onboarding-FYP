58a4ab7d3e26e47cc6d592df852d70332601985d
telamonnaxeno <telamonnaxeno@hotmail.com>
2024-06-03 06:50:56 +0800

    Added charts for individual employees

--- app/Livewire/employee/EmployeeCharts.php
+++ app/Livewire/employee/EmployeeCharts.php
@@ -3,86 +3,176 @@
 namespace App\Livewire\Employee;
 
 use App\Models\User;
+use App\Models\UserSession;
+
 use Asantibanez\LivewireCharts\Facades\LivewireCharts;
 use Asantibanez\LivewireCharts\Models\RadarChartModel;
 use Asantibanez\LivewireCharts\Models\TreeMapChartModel;
 use Livewire\Component;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 
 class EmployeeCharts extends Component
 {
 
-    public $colors = [
-        'Leon Jensen Tan' => '#ff0000',
-        'Lee Jong Suk' => '#0000ff',
-        'Bae Suzy' => '#90cdf4',
-        'Lee Dong Wook' => '#66DA26',
-        'Super Admin' => '#cbd5e0',
-    ];
     public $showDataLabels = false;
     public $firstRun = true;
 
+    //Days to track
+    public $days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ];
+
+    //Day Colors
+    public $dayColors = [
+        'Sunday' => '#9E0031',
+        'Monday' => '#8E0045',
+        'Tuesday' => '#770058',
+        'Wednesday' => '#600047',
+        'Thursday' => '#44001A',
+        'Friday' => '#55172F',
+        'Saturday' => '#642C42',
+    ];
+
     public function render()
     {
-        $users = User::all('id','name');
+        //Currently logged in user
+        $user = auth()->user();
 
-        //Pie Chart
-        $pieChartModel = $users
-        ->reduce(function ($pieChartModel, $data) {
-            $name = $data->name;
-            $value = 1;
-            return $pieChartModel->addSlice($name, $value, '#0000ff'); //TODO: Reprogram for actual purpose $this->colors[$name]
-        }, LivewireCharts::pieChartModel()
-            //->setTitle('Expenses by Type')
+        //Get all current user's sessions
+        $usersessions = UserSession::query()
+        ->join('users', 'user_session.UserID', '=', 'users.id')
+        ->join('profiles', 'users.id', '=', 'profiles.user_id')
+        ->join('companyusers', 'users.id', '=', 'companyusers.UserID')
+        ->orderBy('first_activity_at', 'asc')
+        ->addSelect(DB::raw('DAYNAME(first_activity_at) as DayOfWeek'))
+        ->addSelect(DB::raw('DAYOFWEEK(first_activity_at) as DayNum'))
+        ->where('users.id', '=', $user->id);
+
+        //Data for Bar Chart
+        $barChartData = $usersessions->whereBetween('first_activity_at', [now()->subDays(7)->startOfWeek(Carbon::SUNDAY), now()->subDays(7)->endOfWeek(Carbon::SATURDAY)])
+        ->selectRaw('SEC_TO_TIME(SUM(TIME_TO_SEC(duration))) as total_duration')
+        ->groupBy('DayOfWeek', 'DayNum')->get();
+
+        //Log::info(json_encode($test));
+
+        //Fill in missing days
+        for ($i = 0; $i < count($this->days); $i++) {
+            $day = $this->days[$i];
+
+            //Insert to correct position
+            if (!$barChartData->contains('DayOfWeek', $day)) {
+                $barChartData->push([
+                    'DayOfWeek' => $day,
+                    'DayNum' => $i + 1,
+                    'total_duration' => '00:00:00',
+                ]);
+            }
+        }
+
+        //Time spent on system each day - Bar Chart (Current Week Only)
+        $barChartModel = $barChartData->sortBy('DayNum')
+        ->reduce(function ($barChartModel, $data) {
+
+            $day = $data['DayOfWeek'];
+            $value = round(Carbon::parse($data['total_duration'])->floatDiffInHours('00:00:00'), 2);
+
+            return $barChartModel->addColumn($day, $value, $this->dayColors[$day]);
+
+        }, LivewireCharts::columnChartModel()
+            ->setTitle('Time Spent (Hours)')
             ->setAnimated($this->firstRun)
-            ->setType('donut')
-            ->withOnSliceClickEvent('onSliceClick')
-            //->withoutLegend()
-            ->legendPositionBottom()
-            ->legendHorizontallyAlignedCenter()
-            ->setDataLabelsEnabled($this->showDataLabels)
-            ->setColors($this->colors)
+            ->withoutLegend()
+            ->withoutDataLabels()
         );
 
-        //Line Chart
-        $lineChartModel = $users
-        ->reduce(function ($lineChartModel, $data) use ($users) {
-            $index = $users->search($data);
+        //Data for Line Chart
+        $lineChartData = $usersessions->whereBetween('first_activity_at', [now()->subDays(7)->startOfWeek(Carbon::SUNDAY), now()->subDays(7)->endOfWeek(Carbon::SATURDAY)])
+        ->selectRaw('SEC_TO_TIME(SUM(TIME_TO_SEC(duration))) as total_duration')
+        ->groupBy('DayOfWeek', 'DayNum')->get();
 
-            $amountSum = 10;
+        //Fill in missing days
+        for ($i = 0; $i < count($this->days); $i++) {
+            $day = $this->days[$i];
 
-            if ($index == 2) {
-                $lineChartModel->addMarker(3, $amountSum);
+            //Insert to correct position
+            if (!$lineChartData->contains('DayOfWeek', $day)) {
+                $lineChartData->push([
+                    'DayOfWeek' => $day,
+                    'DayNum' => $i + 1,
+                    'total_duration' => '00:00:00',
+                ]);
             }
+        }
 
-            return $lineChartModel->addPoint($index, rand(0,20), ['id' => $data->id]);
-        }, LivewireCharts::lineChartModel()
+        //Average Engagement
+        $engagementLevel = 0.0;
+
+        //Average Session Length
+        $avgSessionLength = 0.0;
+
+        //Engagement Level - Line Chart (Current Week Only)
+        $lineChartModel = $lineChartData->sortBy('DayNum')
+        ->reduce(function ($lineChartModel, $data) use (&$engagementLevel, &$avgSessionLength) {
+            //Parameter for hours to fulfill each day (How many hours per day should employees engage)
+            $hoursPerDay = 4.0;
+
+            $day = $data['DayOfWeek'];
+
+            //Engagement Percentage
+            $value = round(Carbon::parse($data['total_duration'])->floatDiffInHours('00:00:00'), 2) / $hoursPerDay * 100;
+            $value = round($value, 0);
+            
+            //Add to total engagement level
+            $engagementLevel += $value;
+
+            //Add to average session length
+            $avgSessionLength += Carbon::parse($data['total_duration'])->diffInMilliseconds('00:00:00');
+
+            //Add engagement level
+            $lineChartModel->addSeriesPoint("Engagement (%)", $day, $value, $this->dayColors[$day]);
+
+            //Add minimum engagement level (Marker)
+            return $lineChartModel->addSeriesPoint("Minimum Engagement (%)", $day, round(40.0, 0));
+        }, LivewireCharts::multiLineChartModel()
             ->setAnimated($this->firstRun)
             ->withOnPointClickEvent('onPointClick')
-            ->withLegend()
             ->setSmoothCurve()
-            ->setXAxisVisible(true)
             ->setDataLabelsEnabled(false)
             ->sparklined()
         );
 
+        //Calculate average engagement level
+        $engagementLevel = round($engagementLevel / count($this->days), 0);
+
+        //Set colors based on engagement level
+        if ($engagementLevel < 40) {
+            $lineChartModel
+            ->setColors(['#FF0505', '#5A6A85']);
+        }
+        else {
+            $lineChartModel
+            ->setColors(['#6A1043', '#5A6A85']);
+        }
+
+        //Calculate average session length
+        $avgSessionLength = round($avgSessionLength / count($this->days), 0);
+        $avgSessionLength = Carbon::createFromTimestampMs($avgSessionLength, 'UTC')->format('G:i');
+
         return view('livewire.employee.employee-charts')
         ->with([
-            'pieChartModel' => $pieChartModel,
+            'barChartModel' => $barChartModel,
             'lineChartModel' => $lineChartModel,
+            'engagementLevel' => $engagementLevel,
+            'avgSessionLength' => $avgSessionLength,
         ]);
-
-        // return view('livewire.dashboard')
-        // ->with([
-        //     'columnChartModel' => $columnChartModel,
-        //     'pieChartModel' => $pieChartModel,
-        //     'lineChartModel' => $lineChartModel,
-        //     'areaChartModel' => $areaChartModel,
-        //     'multiLineChartModel' => $multiLineChartModel,
-        //     'multiColumnChartModel' => $multiColumnChartModel,
-        //     'radarChartModel' => $radarChartModel,
-        //     'treeChartModel' => $treeChartModel,
-        // ]);
     }
 }
