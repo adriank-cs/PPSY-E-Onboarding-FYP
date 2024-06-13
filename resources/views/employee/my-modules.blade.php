f25ddd8fbf3b389d54bfc740753dac70a0d0f526
leonjensentan <leonjensentan@gmail.com>
2024-06-13 08:31:14 +0800

    PPSY-42 PPSY-41

--- resources/views/employee/my-modules.blade.php
+++ resources/views/employee/my-modules.blade.php
@@ -15,6 +15,10 @@
             <button class="nav-link" id="completed-tab" data-bs-toggle="tab" data-bs-target="#completed" type="button"
                 role="tab" aria-controls="completed" aria-selected="false">Completed</button>
         </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="overdue-tab" data-bs-toggle="tab" data-bs-target="#overdue" type="button"
+                role="tab" aria-controls="overdue" aria-selected="false">Overdue</button>
+        </li>
     </ul>
 
     <div class="tab-content" id="moduleTabsContent">
@@ -129,11 +133,79 @@
                 @endforeach
             </div>
         </div>
+
+        <!-- Overdue Modules -->
+        <div class="tab-pane fade" id="overdue" role="tabpanel" aria-labelledby="overdue-tab">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="col-md-4">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text">
+                                <i class="ti ti-search"></i>
+                            </span>
+                            <input type="text" class="form-control" id="searchFieldOverdue"
+                                placeholder="Enter Module Name" aria-label="Enter Module Name"
+                                aria-describedby="searchButton">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="row modules-container" id="overdueModulesContainer">
+                @foreach($overdueModules as $module)
+                    <div class="col-md-4">
+                        <div class="card module-card">
+                            <div class="card-body module-card-body">
+                            <a
+                            href="#" class="overdue-module-card">
+                                    <div class="row module-image">
+                                        <img src="{{ $module->image_url }}" alt="Module Photo">
+                                    </div>
+                                    </a>
+                                <div class="row module-title">
+                                    <div class="col-md-12">
+                                    <a
+                                    href="#" class="overdue-module-card"><h5 class="card-title">{{ $module->title }}</h5></a>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="progress-container">
+                                            <div class="progress">
+                                                <div class="progress-bar bg-danger"
+                                                    role="progressbar" style="width: {{ $module->progress }}%;"
+                                                    aria-valuenow="{{ $module->progress }}" aria-valuemin="0"
+                                                    aria-valuemax="100">{{ round($module->progress) }}%</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     </div>
 </div>
 
 <script>
+
+    function showOverdueModal(message) {
+        // Update the modal content
+        document.getElementById('overdueModalMessage').textContent = message;
+        // Show the modal
+        var overdueModal = new bootstrap.Modal(document.getElementById('overdueModal'));
+        overdueModal.show();
+    }
+
+
     $(document).ready(function () {
+
+        $('.overdue-module-card').on('click', function () {
+            showOverdueModal('The module has been overdued and is locked. Please contact your manager for an extension!');
+        });
 
         var $inProgressModulesContainer = $('.modules-container').masonry({
             itemSelector: '.module-card',
@@ -176,6 +248,26 @@
             $completedModulesContainer.masonry('layout');
         });
 
+        var $overdueModulesContainer = $('.modules-container').masonry({
+            itemSelector: '.module-card',
+            columnWidth: '.col-md-4',
+            percentPosition: true
+        });
+
+        $('#searchFieldCompleted').on('input', function () {
+            var searchValue = $(this).val().toLowerCase();
+            $('#overdueModulesContainer .module-card').each(function () {
+                var nameText = $(this).find('.card-title').text().toLowerCase();
+                if (nameText.includes(searchValue)) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
+
+            $overdueModulesContainer.masonry('layout');
+        });
+
         // Reinitialize Masonry layout when a tab is shown
         $('button[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
             if (e.target.id == 'completed-tab') {
@@ -187,6 +279,14 @@
                     $searchField.trigger('input');
                 }, 0);
             } else if (e.target.id == 'in-progress-tab') {
+                var $searchField = $('#searchFieldInProgress');
+                $searchField.val('.');
+                $searchField.trigger('input');
+                setTimeout(function() {
+                    $searchField.val('');
+                    $searchField.trigger('input');
+                }, 0);
+            } else if (e.target.id == 'overdue-tab') {
                 var $searchField = $('#searchFieldInProgress');
                 $searchField.val('.');
                 $searchField.trigger('input');
--- resources/views/includes/modal.blade.php
+++ resources/views/includes/modal.blade.php
@@ -32,3 +32,21 @@
         </div>
     </div>
 </div>
+
+<!-- Overdue Modal -->
+<div class="modal fade" id="overdueModal" tabindex="-1" role="dialog" aria-labelledby="overdueModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="overdueModalLabel">Locked Module</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p id="overdueModalMessage"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
